46e8fa52b8d2abef3feeab7eb04ad2fed963e8df
Khush Bakht Naveed <khush.naveed@dci-student.org>
2025-01-23 13:33:30 +0100

    img update

--- src/pages/HeroContent.jsx
+++ src/pages/HeroContent.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate hook
-import HeroImg from "../assets/images/HeroImage/HeroImg2.mp4";
+import HeroImg from "../assets/images/HeroImage/HeroImg3.mp4";
 
 export default function HeroContent() {
   const words = [
